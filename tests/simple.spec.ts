7f74f04373cb374fb829dd32d1cffd7fe315ab0f
ApoorvaBajpai <apoorvabajpai2424@gmail.com>
2026-03-07 01:19:19 +0530

    CI pipeline updated

--- tests/simple.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Simple check', async ({ page }) => {
-    await page.goto('http://localhost:5174/auth');
-    await expect(page.locator('h2')).toContainText(['Login', 'Sign Up']);
-});
